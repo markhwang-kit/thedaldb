483d23882ec22e4425d1035d0de09d0f64da65fb
802 <802@DESKTOP-O9V7DCF>
2020-07-23 16:42:55 +0900

    UserDao 기능 완성 하고 테스트 중

--- src/dao/UserDaoImpl.java
+++ src/dao/UserDaoImpl.java
@@ -50,14 +50,90 @@ public class UserDaoImpl implements UserDao{
 
 	@Override
 	public UserDto select(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		// 전달 변수(dto 담을 그릇)
+		UserDto dto = null;
+		try {
+			conn = DB.conn();
+//			String sql = "SELECT * FROM user WHERE  id= '" + id + "'";
+			String sql = "SELECT * FROM user WHERE  id= ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				dto = new UserDto();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setJumsu(rs.getInt("jumsu"));
+			} else {
+				System.out.println("사용자가 없다");
+			}
+
+		} catch (Exception e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if( rs != null && !rs.isClosed()){
+                    rs.close();
+                }
+				if( pstmt != null && !pstmt.isClosed()){
+                    pstmt.close();
+                }
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return dto;
 	}
 
 	@Override
 	public ArrayList<UserDto> select() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		// 전달 변수(dto 담을 그릇)
+		ArrayList<UserDto> list = new ArrayList<UserDto>();
+		try {
+			conn = DB.conn();
+			String sql = "SELECT * FROM user";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				UserDto dto = new UserDto();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setJumsu(rs.getInt("jumsu"));
+				list.add(dto);
+			}
+
+		} catch (Exception e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if( rs != null && !rs.isClosed()){
+                    rs.close();
+                }
+				if( pstmt != null && !pstmt.isClosed()){
+                    pstmt.close();
+                }
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 
 	@Override
--- src/game/MainClass.java
+++ src/game/MainClass.java
@@ -1,9 +1,27 @@
 package game;
 
+import java.util.Scanner;
+
+import dao.UserDao;
+import dao.UserDaoImpl;
+import dto.UserDto;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("디비 게임");		
+		System.out.println("디비 게임");
+		System.out.println("아이디 있으면 아이디 입력\n없으면 가입하세요.");
+		System.out.print("입력: ");
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();   // id 입력 받기
+		System.out.println("당신이 입력한 ID: " + id);
+		UserDao userDao = new UserDaoImpl();
+		UserDto dto = userDao.select(id);
+		if (dto == null) {
+			System.out.println("데이터가 없다");
+		} else {
+			System.out.println("데이터가 있다");
+		}
 	}
 
 }
