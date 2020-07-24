0c7a602afc356bdf895fd745ed3c632948f3bfd2
802 <802@DESKTOP-O9V7DCF>
2020-07-24 09:51:02 +0900

    ID체크 가입 구현 점수 출력까지 테스트 완료

--- src/game/MainClass.java
+++ src/game/MainClass.java
@@ -18,10 +18,28 @@ public class MainClass {
 		UserDao userDao = new UserDaoImpl();
 		UserDto dto = userDao.select(id);
 		if (dto == null) {
-			System.out.println("데이터가 없다");
-		} else {
-			System.out.println("데이터가 있다");
+			// dto 가 null 이기 때문에 new 로 공간을 만들어야함.
+			dto = new UserDto();
+			System.out.println("아이디가 없습니다");
+			System.out.println("회원가입을 시작합니다.");
+			System.out.print("이름을 입력하세요: ");
+			String name = sc.next();
+			// id와 이름을 dto 에 셋팅
+			dto.setId(id);
+			dto.setName(name);
+			// DB에 insert를 한다.
+			userDao.insert(dto);
 		}
+//		} else {
+//			System.out.println("데이터가 있다");
+//		
+//		}
+		
+		// 유저 정보 출력
+		System.out.println("당신의 정보입니다.");
+		System.out.println("아이디: " + dto.getId());
+		System.out.println("이름: " + dto.getName());
+		System.out.println("점수: " + dto.getJumsu());
 	}
 
 }
